520e2788f48af5e3742441edfb137d119c50e9ed
Erik Kauê <kaue_erik122@hotmail.com>
2023-08-28 16:00:03 -0300

    Changing the state of the application according to the action

--- Scripts/FocusTimer/actions.js
+++ Scripts/FocusTimer/actions.js
@@ -1,7 +1,14 @@
+import state from './state.js'
+
 export function toggleRunning () {
-    console.log('toggleRunning Function')
+    state.isRunning = document.documentElement.classList.toggle('running')
 }
 
 export function set () {
-    
+    state.isRunning = false
+    document.documentElement.classList.remove('runing')
+}
+
+export function toggleMusic(){
+    state.isMute = document.documentElement.classList.toggle('music-on')
 }
